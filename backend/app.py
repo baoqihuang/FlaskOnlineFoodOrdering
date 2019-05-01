2172e9437299371c5e3b35fe36af170b9b2f0e95
Michael Huang <hbqljy@gmail.com>
2019-04-30 17:11:54 -0700

    apis

--- backend/app.py
+++ backend/app.py
@@ -4,8 +4,8 @@ from flask_jwt_extended import create_access_token, JWTManager, get_jwt_identity
 from flask_bcrypt import Bcrypt
 from pathlib import Path
 from flask_cors import CORS
-
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime,timedelta
 f_jwt = JWTManager()
 
 
@@ -20,7 +20,7 @@ def check_if_token_in_blacklist(decrypted_token):
 
 class Config:
     project_dir = os.path.dirname(os.path.abspath(__file__))
-    database_file = "sqlite:///{}".format(os.path.join(project_dir, "cs157b.db"))
+    database_file = "sqlite:///{}".format(os.path.join(project_dir, "usersinfo.db"))
     SQLALCHEMY_DATABASE_URI = database_file
     SECRET_KEY = 'dev'
     FLASK_ENV = 'development'
@@ -35,6 +35,43 @@ bcrypt = Bcrypt(app)
 f_jwt.init_app(app)
 
 
+class Itemsinorder(db.Model):
+    order_id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, order_id=None, item_id=None, quantity=None):
+        self.order_id = order_id
+        self.item_id = item_id
+        self.quantity = quantity
+
+    def __repr__(self):
+        return '<Order %r>' % self.id
+
+    def to_dict(self):
+        result = {"order_id": self.order_id, "item_id": self.item_id, "quantity": self.quantity}
+        return result
+
+
+class Shoppingcart(db.Model):
+    user_id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer, primary_key=True)
+    quantity = db.Column(db.Integer, nullable=False)
+
+    def __init__(self, user_id=None, item_id=None, quantity=None):
+        self.user_id = user_id
+        self.item_id = item_id
+        self.quantity = quantity
+
+    def __repr__(self):
+        return '<Shoppingcart %r>' % self.user_id
+
+
+    def to_dict(self):
+        result = {"order_id": self.order_id, "item_id": self.item_id, "quantity": self.quantity}
+        return result
+
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first = db.Column(db.String(50), unique=False, nullable=False)
@@ -43,20 +80,77 @@ class User(db.Model):
     username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(50), unique=True, nullable=False)
     user_type = db.Column(db.String(50), unique=False, nullable=False)
+    phonenumber = db.Column(db.String(15), unique=False, nullable=True)
+    orders = db.relationship('Order', backref='user', lazy=True)
 
-    def __init__(self, first=None, last=None, email=None, username=None, password=None, user_type=None):
+    def __init__(self, first=None, last=None, email=None, username=None, password=None, user_type=None, phonenumber=None):
         self.first = first
         self.last = last
         self.email = email
         self.username = username
         self.password = password
         self.user_type = user_type
+        self.phonenumber = phonenumber
 
     def __repr__(self):
         return '<User %r>' % self.username
 
     def to_dict(self):
-        result = {"first": self.first, "last": self.last, "email": self.email, "username": self.username}
+        result = {"first": self.first, "last": self.last, "email": self.email, "username": self.username, "user_type": self.user_type, "phonenumber": self.phonenumber}
+        return result
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    total = db.Column(db.Float, unique=False, nullable=False)
+    time = db.Column(db.DateTime, unique=False, nullable=False)
+    ifcancelable = db.Column(db.Boolean, nullable=False)
+    ifpickup = db.Column(db.Boolean, nullable=False)
+    status = db.Column(db.String(20), unique=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __init__(self, total=None, time=None, ifcancelable=None, ifpickup=None, status=None, user_id=None):
+        self.total = total
+        self.time = time
+        self.ifcancelable = ifcancelable
+        self.ifpickup = ifpickup
+        self.status = status
+        self.user_id = user_id
+
+    def __repr__(self):
+        return '<Order %r>' % self.id
+
+    def to_dict(self):
+        result = {"id": self.id, "total": self.total, "time": self.time, "ifcancelable": self.ifcancelable, "ifpickup": self.ifpickup, "status": self.status, "user_id": self.user_id}
+        return result
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=False, nullable=False)
+    price = db.Column(db.Float)
+    description = db.Column(db.String(1000), unique=False, nullable=False)
+    ingredient = db.Column(db.String(1000), unique=False, nullable=False)
+    cal = db.Column(db.Integer, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    picurl = db.Column(db.String(100), unique=True, nullable=False)
+    category = db.Column(db.String(15), unique=False, nullable=False)
+
+    def __init__(self, name=None, price=None, description=None, ingredient=None, cal=None, rating=None, picurl=None, category=None):
+        self.name = name
+        self.price = price
+        self.description = description
+        self.ingredient = ingredient
+        self.cal = cal
+        self.rating = rating
+        self.picurl = picurl
+        self.category = category
+
+    def __repr__(self):
+        return '<Item %r>' % self.id
+
+    def to_dict(self):
+        result = {"id": self.id, "name": self.name, "price": self.price, "description": self.description, "ingredient": self.ingredient, "cal": self.cal, "rating": self.rating, "picurl": self.picurl, "category": self.category}
         return result
 
 # Catch all
@@ -94,7 +188,7 @@ def login_user():
     if not valid:
         return jsonify({'msg': 'Invalid username/password'}), 409
 
-    token = create_access_token(identity={'username': username, 'user_type': client.user_type})
+    token = create_access_token(identity={'username': username})
     return jsonify({'access_token': token}), 201
 
 
@@ -142,17 +236,18 @@ def register_user():
         email = data["email"].lower()
         username = data["username"].lower()
         password = data['password']
-        user_type = data.get('user_type', 'client')
+        user_type = data.get('user_type', 'manager')
+        phonenumber = data.get('phonenumber', '8888888888')
     except KeyError:
         return jsonify({'msg': 'Bad Request, missing/misspelled key'}), 400
 
-    user = User.query.filter_by(username=username, email=email).first()
+    user = User.query.filter_by(username=username).filter_by(email=email).first()
     if user is None:
         pw_hash = bcrypt.generate_password_hash(password.encode('UTF-8'))
-        newuser = User(first, last, email, username, pw_hash, user_type)
+        newuser = User(first, last, email, username, pw_hash, user_type, phonenumber)
         db.session.add(newuser)
         db.session.commit()
-        return redirect("/")
+        return jsonify({'msg': 'User register success'}), 200
     else:
         return jsonify({'msg': 'User already existed'}), 400
 
@@ -166,6 +261,335 @@ def get_all_detail():
         return jsonify({'msg': 'client not found'}), 409
 
     return jsonify(client.to_dict()), 200
+
+
+@app.route('/deleteuser', methods=['GET'])
+@jwt_required
+def delete_user():
+    current_user = get_jwt_identity()['username']
+    client = User.query.filter_by(username=current_user).first()
+    if not client:
+        return jsonify({'msg': 'client not found'}), 409
+    else:
+        db.session.delete(client)
+        db.session.commit()
+    return jsonify({'msg': 'user deleted successfully'}), 200
+
+
+# display all the items'id,name,price,picture before login
+@app.route('/display', methods=['GET'])
+# @jwt_required
+def display_all():
+    items = Item.query.all()
+    if not items:
+        return jsonify({'msg': 'No item in the databse'}), 409
+
+    result = []
+    for item in items:
+        result.append({"id": item.id, "name": item.name, "price": item.price, "picture": item.picurl})
+    return jsonify(result), 200
+
+# display the specific item'id,name,price,picture before login
+@app.route('/display/detail', methods=['POST'])
+# @jwt_required
+def display_detail():
+    data = request.get_json()
+    if not data:
+        return jsonify({'msg': 'Bad Request, no data passed'}), 400
+
+    try:
+        item_id = data["item_id"]
+    except KeyError:
+        return jsonify({'msg': 'Bad Request, missing/misspelled key'}), 400
+
+    item = Item.query.filter_by(id=item_id).first()
+    if item is None:
+        return jsonify({'msg': 'Item is not existed'}), 400
+    else:
+        return jsonify(item.to_dict()), 200
+
+
+@app.route('/setupitems', methods=['POST'])
+# @jwt_required
+def set_up_items():
+    data = request.get_json()
+    if not data:
+        return jsonify({'msg': 'Bad Request, no data passed'}), 400
+    name = data["name"].lower()
+    price = data["price"]
+    description = data["description"].lower()
+    ingredient = data["ingredient"].lower()
+    cal = data['cal']
+    rating = data["rating"]
+    picurl = data["picurl"].lower()
+    category = data['category'].lower()
+
+    item = Item.query.filter_by(name=name).first()
+    if item is None:
+        item = Item(name, price, description, ingredient, cal, rating, picurl, category)
+        db.session.add(item)
+        db.session.commit()
+        return jsonify({"msg": "Successfully add item"}), 200
+    else:
+        return jsonify({'msg': 'Item already existed'}), 400
+
+
+@app.route('/auth/userorderhistory', methods=['GET'])
+@jwt_required
+def get_all_orderhistory():
+    current_user = get_jwt_identity()['username']
+    client = User.query.filter_by(username=current_user).first()
+    if not client:
+        return jsonify({'msg': 'client not found'}), 409
+    orders = Order.query.filter_by(user_id=client.id).all()
+    if not orders:
+        return jsonify({'msg': "This client's order does not found"}), 409
+    userorders = []
+    for order in orders:
+        userorders.append({'id': order.id, 'status': order.status, 'time': order.time, 'total': order.total})
+
+    return jsonify(userorders), 200
+
+
+@app.route('/auth/orderdetail', methods=['POST'])
+@jwt_required
+def display_order_detail():
+    current_user = get_jwt_identity()['username']
+    client = User.query.filter_by(username=current_user).first()
+    if not client:
+        return jsonify({'msg': 'client not found'}), 409
+
+    data = request.get_json()
+    if not data:
+        return jsonify({'msg': 'Bad Request, no data passed'}), 400
+
+    try:
+        order_id = data["order_id"]
+    except KeyError:
+        return jsonify({'msg': 'Bad Request, missing/misspelled key'}), 400
+    order = Order.query.filter_by(id=order_id).first()
+    timefornow = datetime.now()
+    minustentime = timefornow - timedelta(minutes=10)
+    if order.time < minustentime:
+        order.ifcancelable = False
+        db.session.commit()
+    itemsid = Itemsinorder.query.filter_by(order_id=order_id).all()
+    if not itemsid:
+        return jsonify({'msg': "This order does not have items"}), 409
+
+    orderitems = []
+    for itemid in itemsid:
+        item = Item.query.filter_by(id=itemid.item_id).first()
+        orderitems.append({'name': item.name, 'price': item.price, 'quantity': itemid.quantity,
+                           'picurl': item.picurl})
+
+    return jsonify({"order_status": order.status, "ifcancellable": order.ifcancelable, "orderdetail": orderitems}), 200
+
+
+@app.route('/auth/cancelorder', methods=['POST'])
+@jwt_required
+def order_cancel():
+    current_user = get_jwt_identity()['username']
+    client = User.query.filter_by(username=current_user).first()
+    if not client:
+        return jsonify({'msg': 'client not found'}), 409
+
+    data = request.get_json()
+    if not data:
+        return jsonify({'msg': 'Bad Request, no data passed'}), 400
+
+    try:
+        order_id = data["order_id"]
+    except KeyError:
+        return jsonify({'msg': 'Bad Request, missing/misspelled key'}), 400
+
+    order = Order.query.filter_by(order_id=order_id).first()
+    if not order:
+        return jsonify({'msg': "This order does not exist"}), 409
+    else:
+        db.session.delete(order)
+        db.session.commit()
+        itemsid = Itemsinorder.query.filter_by(order_id=order_id).first()
+        for itemid in itemsid:
+            db.session.delete(itemid)
+            db.session.commit()
+
+    return jsonify({'msg': "This order is cancelled"}), 200
+
+
+@app.route('/auth/manager', methods=['GET'])
+@jwt_required
+def manager_display():
+    current_user = get_jwt_identity()['username']
+    client = User.query.filter_by(username=current_user).first()
+    if not client or client.user_type != 'manager':
+        return jsonify({'msg': 'Invalid client or client not found'}), 409
+    pickuporders = []
+    orders = Order.query.filter_by(ifplaceedorder=True, ifpickup=False).first()
+    if not orders:
+        return jsonify({'msg': "This is no order need to be pick up"}), 409
+    else:
+        for order in orders:
+            itemsid = Itemsinorder.query.filter_by(order_id=order.id).all()
+            detail = ""
+            for itemid in itemsid:
+                item = Item.query.filter_by(id=itemid.item_id).first()
+                detail += item.name + " * " + str(itemid.quantity) + " "
+            pickuporders.append({"order_id": order.id, "detail":detail, "order_time": order.time})
+    return jsonify(pickuporders), 200
+
+
+@app.route('/auth/managerpickup', methods=['POST'])
+@jwt_required
+def manager_pickup():
+    current_user = get_jwt_identity()['username']
+    client = User.query.filter_by(username=current_user).first()
+    if not client:
+        return jsonify({'msg': 'Invalid client or client not found'}), 409
+
+    data = request.get_json()
+    if not data:
+        return jsonify({'msg': 'Bad Request, no data passed'}), 400
+
+    try:
+        order_id = data["order_id"]
+    except KeyError:
+        return jsonify({'msg': 'Bad Request, missing/misspelled key'}), 400
+
+    order = Order.query.filter_by(id=order_id).first()
+    if not order:
+        return jsonify({'msg': "This is no order need to be pick up"}), 409
+    else:
+        order.ifcancelable = False
+        order.status = "pickup"
+    return jsonify({'msg': "Order pick up successfully"}), 200
+
+
+@app.route('/auth/shoppingcart', methods=['GET'])
+@jwt_required
+def user_shoppingcart():
+    current_user = get_jwt_identity()['username']
+    client = User.query.filter_by(username=current_user).first()
+    if not client:
+        return jsonify({'msg': 'Invalid client or client not found'}), 409
+
+    itemsid = Shoppingcart.query.filter_by(user_id=client.id).all()
+    if not itemsid:
+        return jsonify({'msg': "Nothing in the shopping cart"}), 409
+    itemsinfo = []
+    for itemid in itemsid:
+        item = Item.query.filter_by(id=itemid.item_id).first()
+        itemsinfo.append({"picurl": item.picurl, "name": item.name, "description": item.description, "price": item.price})
+    return jsonify(itemsinfo), 200
+
+
+@app.route('/auth/shoppingcarttotal', methods=['GET'])
+@jwt_required
+def user_shoppingcarttotal():
+    current_user = get_jwt_identity()['username']
+    client = User.query.filter_by(username=current_user).first()
+    if not client:
+        return jsonify({'msg': 'Invalid client or client not found'}), 409
+
+    itemsid = Shoppingcart.query.filter_by(user_id=client.id).all()
+    if not itemsid:
+        return jsonify({'msg': "This is no order need to be pick up"}), 409
+    total = 0
+    for itemid in itemsid:
+        item = Item.query.filter_by(id=itemid.item_id).first()
+        total += item.price * itemid.quantity
+    return jsonify({"total": total}), 200
+
+
+@app.route('/auth/shoppingcart/additem', methods=['POST'])
+@jwt_required
+def shoppingcart_additem():
+    current_user = get_jwt_identity()['username']
+    client = User.query.filter_by(username=current_user).first()
+    if not client:
+        return jsonify({'msg': 'Invalid client or client not found'}), 409
+
+    data = request.get_json()
+    if not data:
+        return jsonify({'msg': 'Bad Request, no data passed'}), 400
+
+    try:
+        item_id = data["item_id"]
+    except KeyError:
+        return jsonify({'msg': 'Bad Request, missing/misspelled key'}), 400
+
+    item = Shoppingcart.query.filter_by(user_id=client.id, item_id=item_id).first()
+
+    if not item:
+        newiteminshippingcart = Shoppingcart(client.id, item_id, 1)
+        db.session.add(newiteminshippingcart)
+    else:
+        item.quantity += 1
+
+    db.session.commit()
+
+    return jsonify({'msg': 'Item add to the shoopingcart successfully'}), 200
+
+
+@app.route('/auth/shoppingcart/deleteitem', methods=['POST'])
+@jwt_required
+def shoppingcart_deleteitem():
+    current_user = get_jwt_identity()['username']
+    client = User.query.filter_by(username=current_user).first()
+    if not client:
+        return jsonify({'msg': 'Invalid client or client not found'}), 409
+
+    data = request.get_json()
+    if not data:
+        return jsonify({'msg': 'Bad Request, no data passed'}), 400
+
+    try:
+        item_id = data["item_id"]
+    except KeyError:
+        return jsonify({'msg': 'Bad Request, missing/misspelled key'}), 400
+
+    item = Shoppingcart.query.filter_by(user_id=client.id, item_id=item_id).first()
+
+    if not item:
+        return jsonify({'msg': 'Item does not existed in the shoppingcart'}), 400
+    else:
+        if item.quantity <= 1:
+            db.session.delete(item)
+        else:
+            item.quantity -= 1
+        db.session.commit()
+    return jsonify({'msg': 'Item was deleted from the shoopingcart successfully'}), 200
+
+
+@app.route('/auth/shoppingcart/placeorder', methods=['GET'])
+@jwt_required
+def shoppingcart_placeorder():
+    current_user = get_jwt_identity()['username']
+    client = User.query.filter_by(username=current_user).first()
+    if not client:
+        return jsonify({'msg': 'Invalid client or client not found'}), 409
+
+    itemsid = Shoppingcart.query.filter_by(user_id=client.id).all()
+    if not itemsid:
+        return jsonify({'msg': "This is no order need to be pick up"}), 409
+    total = 0
+    for itemid in itemsid:
+        item = Item.query.filter_by(id=itemid.item_id).first()
+        total += item.price * itemid.quantity
+    # set timer to 10
+    ordertime = datetime.now()
+    neworder = Order(total, ordertime, True, False, "inprogress", client.id)
+    db.session.add(neworder)
+    db.session.commit()
+    for itemid in itemsid:
+        iteminorder = Itemsinorder(neworder.id, itemid.item_id, itemid.quantity)
+        db.session.add(iteminorder)
+        db.session.commit()
+
+    for itemid in itemsid:
+        db.session.delete(itemid)
+        db.session.commit()
+    return jsonify({'msg': 'Order checkout successfully'}), 200
 
 
 if __name__ == '__main__':
